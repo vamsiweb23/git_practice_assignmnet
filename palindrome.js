60526eb8f179d0d3190194136c01083f1fa948a8
vamsiweb23 <vvs123109@gmail.com>
2022-10-19 21:40:42 +0530

    folder

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,17 @@
 //palindrome problem
 var str="vamsi"
 //enter  code here
+
+var bag=""
+for(var i=str.length-1; i>=0; i--)
+{
+    bag+=str[i]
+    
+}
+if(bag==str)
+{
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
--- prime.js
+++ prime.js
@@ -1,3 +1,20 @@
 //check prime
 var num=13;
 //type code here 
+
+
+var count=0;
+for(var i=2; i<=num**0.5; i++)
+{
+    if(num%i==0)
+    {
+        count++
+    }
+}
+if(count==0)
+{
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
